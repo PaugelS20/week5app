1f9bde5b7d2c98f1ed0f1f7a8a1966df99c1db3e
Sam Paugel <shpaugel@madisoncollege.edu>
2023-02-08 21:26:26 -0600

    added more props to the component
    favorite number

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,26 +1,20 @@
 import React from 'react'
 
-export const Greeting = ({ props }) => {
+export const Greeting = (props) => {
   return (
     <>
-    <h1>
-        Hello {props.name}!!!
-    </h1>
-    <h2>
-        Hello {props.name}!!!
-    </h2>
-    <h2>
-        {props.name}'s favorite number is {props.favoriteNumber}
-    </h2>
-    <h2>
-        {props.name}'s favorite number plus 10 is {}
-    </h2>
-    <h2>
-        {props.name}'s favorite colors are {props.favoriteColors}
-    </h2>
-    <h2>
-        Hello {props.name}!!!
-    </h2>
+        <h2>
+            Hello {props.name}!!!
+        </h2>
+    
+        <h3>
+            {props.name}'s favorite number is {props.favoriteNumber}
+        </h3>
+    
+        <h3>
+            {props.name}'s favorite number plus 10 is { props.favoriteNumber + 10 }
+        </h3>
+
     </>
   );
 };
