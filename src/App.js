3f7872645cc4646332bbcdf2b444c246f7797dcb
Sam Paugel <shpaugel@madisoncollege.edu>
2023-02-08 22:03:43 -0600

    added date time
    To change color depending on the time if devisable by 2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 import { Greeting } from './Greeting';
 
 const App = () => {
+  const styles = {
+    container: {
+      padding: 20,
+      backgroundColor: new Date().getSeconds() % 2 == 0 ? "lightyellow" : "blue"
+    }
+  }
   return (
-  <> 
+  <div style={styles.container}> 
       <Greeting name="React" favoriteNumber={37} favoriteColors={["red","blue","brass"]}/>
       <Greeting name="Sam" favoriteNumber={100}/>
       <Greeting name="Matt" favoriteNumber={27}/>
       <Greeting name="Amy" favoriteNumber={45}/>
-  </>
+  </div>
 
   );
 }
